d9cea0dc366a349fed3962de71742c311703ec68
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2025-07-29 20:43:30 +0100

    Modification de frontend/src/layouts/AppLayout.tsx

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -36,7 +36,7 @@ const AppLayout: React.FC = () => {
     },
     {
       name: 'Mes notes',
-      path: '/notes',
+      path: '/notes/own',
       icon: (
         <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
@@ -45,7 +45,7 @@ const AppLayout: React.FC = () => {
     },
     {
       name: 'Notes partagées',
-      path: '/shared',
+      path: '/notes/shared',
       icon: (
         <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.367 2.684 3 3 0 00-5.367-2.684z" />
